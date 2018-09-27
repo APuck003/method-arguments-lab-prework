5cef514c94f0b46bdc62656be6c72a360ad6a4ca
Austin Puckett <puckett_austin@yahoo.com>
2018-09-27 06:38:28 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,10 @@
 name = Austin
 language = Ruby
 
-def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
